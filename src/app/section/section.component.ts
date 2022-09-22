c2ff88a678d00af39fe7fdfaad3a1b4c09d1ca9b
Clemens Meyer zu Rheda <clemens.mzr@gmail.com>
2022-09-22 18:15:06 +0200

    Created simple section component

---
+++ src/app/section/section.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-section',
+  templateUrl: './section.component.html',
+  styleUrls: ['./section.component.css']
+})
+export class SectionComponent implements OnInit {
+
+  @Input() title: string = "";
+  @Input() subtitle: string = "";
+
+  @Input() isTextarea: boolean = true;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
