f94dd001e6cbb2116ef5541d08998f688c41b2d6
Clemens Meyer zu Rheda <clemens.meyerzurheda@tech4germany.org>
2022-09-27 10:06:19 +0200

    Firebase configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAGsvotwmjzw0BOPiern-OlRoYYKssubYo",
+    authDomain: "t4g22-prototype.firebaseapp.com",
+    projectId: "t4g22-prototype",
+    storageBucket: "t4g22-prototype.appspot.com",
+    messagingSenderId: "57982292189",
+    appId: "1:57982292189:web:f3d9a1f9fef7b5b7efcb34",
+    measurementId: "G-CW34DBW7DG"
+  },
 };
